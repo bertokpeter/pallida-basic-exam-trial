2dbe65b625047975edf72c126a9b4c8f25ce6cfc
bertokpeter <bertokpeter92@gmail.com>
2017-09-19 14:54:17 +0200

    feat(name_from_email):wrong input message added

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -4,17 +4,18 @@
 # last_name first_name
 # example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
 # accents does not matter
-
+email_address = input("Please enter your email address here (e.g elek.viz@exam.com): ")
 def name_from_email(email_address):
-    # if not "@" in email_address:
-    #     print
-    dot = email_address.find(".")
-    at = email_address.find("@")
-    first_name = "" + email_address[:dot]
-    last_name = "" + email_address[dot + 1:at]
-    user_name = "" + capitalizer(last_name) + " " + capitalizer(first_name)
-    return user_name
+    if not "@exam.com" in email_address:
+        return "Your email address is not correct"
+    else:
+        dot = email_address.find(".")
+        at = email_address.find("@")
+        first_name = "" + email_address[:dot]
+        last_name = "" + email_address[dot + 1:at]
+        user_name = "" + capitalizer(last_name) + " " + capitalizer(first_name)
+        return user_name
 def capitalizer(name):
     name = name.capitalize()
     return name
-print(name_from_email("elek.viz@exam.com"))
+print(name_from_email(email_address))
